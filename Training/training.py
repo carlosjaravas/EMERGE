e84f563f0b8fddf8e19e1ab540fdf2709c06cb06
carlosjaravas <carlosjaravas@gmail.com>
2023-10-07 13:21:40 +0200

    Main training.py updated to merge
    Multiple seq with multi steps working and updated in main file

--- Training/training.py
+++ Training/training.py
@@ -4,16 +4,17 @@ import pickle
 from zmqRemoteApi import RemoteAPIClient
 from random import randint as rn
 
-#---------------------------------------------------Class creation
-
 #Class for simulation object
 class sim_objects ():
     def __init__(self):
-        self.client = 0
-        self.sim = 0
+        self.client = RemoteAPIClient()
+        self.sim = self.client.getObject('sim')
         self.file = 0
         self.random_increments = []
         self.executed = False
+        self.handler_ids = []
+        self.sequencies = 1
+        self.seq_steps = 2
 
 sim_obj = sim_objects()
 
@@ -36,38 +37,14 @@ class handlers_class ():
 
 handler = handlers_class()
 
-#Class to store all the perceptions
-class perceptions ():
-    def __init__(self):
-        #Initial perceptions
-        self.prev_j_positions = []
-        self.prev_j_velocities = []
-        self.prev_j_l_velocities = []
-        self.prev_j_forces = []
-        self.prev_base_pos = []
-        self.prev_base_ori = []
-        self.prev_base_accel = []
-
-        #Resulting perceptions
-        self.post_j_positions = []
-        self.post_j_velocities = []
-        self.post_j_l_velocities = []
-        self.post_j_forces = []
-        self.post_base_pos = []
-        self.post_base_ori = []
-        self.post_base_accel = []
-
-perception = perceptions()
-
-#--------------------------------------------------------Functions
-
 # Function that load the robot in the scene at the beggining of each evaluation.
 def load_quad_class():
     # Opening the connection with CoppeliaSim simulator
     sim_obj.client = RemoteAPIClient()
     sim_obj.sim = sim_obj.client.getObject('sim')
     
-    # Setting the simulation mode for working in sinchronous mode. The simulation timming is controlled by the Python program and not by the simulation itselfs.
+    # Setting the simulation mode for working in sinchronous mode. The simulation 
+    # timming is controlled by the Python program and not by the simulation itselfs.
     sim_obj.client.setStepping(True)
     
     handler.base = sim_obj.sim.getObject("/Base")
@@ -87,16 +64,43 @@ def load_quad_class():
     handler.JLB0 = sim_obj.sim.getObject("/JLB0")
     handler.JLB1 = sim_obj.sim.getObject("/JLB1")
     #handler.JLB2 = sim_obj.sim.getObject("/JLB2")
+    
+    #Stores the different id for every handler starting with the base
+    sim_obj.handler_ids = list(handler.__dict__.values())
 
-#Function to generate list of random positions
+    sim_obj.sim.startSimulation()
+
+#Generate list of random positions
 def rand_gen():
     randomlist = []
-    for i in range(9):
+    for i in range(len(sim_obj.handler_ids)-1):
         n = rn(-10, 10) * math.pi / 180
         randomlist.append(n)
     sim_obj.random_increments.append(randomlist)
 
-#Function to get perceptions-----------base acceleration is missing
+#Class to store all the perceptions
+class perceptions ():
+    def __init__(self):
+        #Initial perceptions
+        self.prev_j_positions = []
+        self.prev_j_velocities = []
+        self.prev_j_l_velocities = []
+        self.prev_j_forces = []
+        self.prev_base_pos = []
+        self.prev_base_ori = []
+        self.prev_base_accel = []
+
+        #Resulting perceptions
+        self.post_j_positions = []
+        self.post_j_velocities = []
+        self.post_j_l_velocities = []
+        self.post_j_forces = []
+        self.post_base_pos = []
+        self.post_base_ori = []
+        self.post_base_accel = []        
+
+perception = perceptions()
+
 def get_joints_preceptions():
     positions = []
     forces = []
@@ -104,8 +108,8 @@ def get_joints_preceptions():
     l_velocities = []
     base_pos = sim_obj.sim.getObjectPosition(handler.base, sim_obj.sim.handle_world)
     base_ori = sim_obj.sim.getObjectOrientation(handler.base, sim_obj.sim.handle_world)
-    base_accel = [] #sim_obj.sim.getObjectOrientation(handler.base, sim_obj.sim.handle_world) --- MISSING
-    for i in handler.__dict__.values():
+    base_accel = [0,0,0] #sim_obj.sim.getObjectOrientation(handler.base, sim_obj.sim.handle_world) --- MISSING
+    for i in sim_obj.handler_ids[:-1]:
         #Angular/linear position of the joint
         pos = sim_obj.sim.getJointPosition(i)
         positions.append(pos)
@@ -118,8 +122,8 @@ def get_joints_preceptions():
         #Linear velocity of the joint
         l_veloc = sim_obj.sim.getJointVelocity(i)
         l_velocities.append(l_veloc)
-    #Determines if its a prev/post perception
     if  not sim_obj.executed:
+        print("Getting prev perceptions")
         perception.prev_j_positions.append(positions)
         perception.prev_j_velocities.append(velocities)
         perception.prev_j_l_velocities.append(l_velocities)
@@ -128,6 +132,7 @@ def get_joints_preceptions():
         perception.prev_base_ori.append(base_ori)
         perception.prev_base_accel.append(base_accel)
     else:
+        print("Getting post perceptions")
         perception.post_j_positions.append(positions)
         perception.post_j_velocities.append(velocities)
         perception.post_j_l_velocities.append(l_velocities)
@@ -136,30 +141,41 @@ def get_joints_preceptions():
         perception.post_base_ori.append(base_ori)
         perception.post_base_accel.append(base_accel)
 
-
-#Main function
-def main():
-    sim_obj.sim.startSimulation()
-    load_quad_class()
-    rand_gen()
-    get_joints_preceptions()
-    #Execute every joint action
-    for i in range(len(sim_obj.random_increments[-1])):
-        sim_obj.sim.setJointTargetPosition(handler.__dict__.values()[i], (sim_obj.random_increments[-1][i]+perception.prev_j_positions[-1][i]))
-        #4 steps let the joints get to their target position with the less velocity possible (1 step = 0.05s)
-        sim_obj.client.step()
-        sim_obj.client.step()
-        sim_obj.client.step()
-        sim_obj.client.step()
-    sim_obj.executed = True
-    get_joints_preceptions()
-    sim_obj.executed = False
-    sim_obj.sim.stopSimulation()
+def export():    
     #Dump data in file
-    sim_obj.file = open(os.getcwd() + r'\training.txt', 'wb')
+    sim_obj.file = open(os.getcwd() + r'\training_dataset.txt', 'wb')
     pickle.dump(perception, sim_obj.file)
     sim_obj.file.close()
 
+#Main function for multiple executions and multiple steps
+def main():
+    print("main function")
+    for k in range(sim_obj.sequencies):
+        print(f"{k} sequence has started")
+        print("")
+        #sim_obj.sim.startSimulation()
+        load_quad_class()
+        for j in range(sim_obj.seq_steps):
+            print("----------- Secuencia: ", k)
+            print("----------- Paso: ", j)
+            rand_gen()
+            get_joints_preceptions()
+            #Execute every joint action
+            print("Executing actions")
+            for i in range(len(sim_obj.random_increments[-1])):
+                sim_obj.sim.setJointTargetPosition(sim_obj.handler_ids[i], (sim_obj.random_increments[-1][i]+perception.prev_j_positions[-1][i]))
+                sim_obj.client.step()
+                sim_obj.client.step()
+                sim_obj.client.step()
+                sim_obj.client.step()
+            sim_obj.executed = True
+            get_joints_preceptions()
+            sim_obj.executed = False
+            print("")
+        print(f"{k} simulation has finished")
+        print("")
+        sim_obj.sim.stopSimulation()
+    export()
 
 if __name__ == "__main__":
     main()
